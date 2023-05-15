c8e3847439643138a084af60f349ad5eb19e1bcb
Tarif M <tarif.m@zohocorp.com>
2023-05-15 16:54:14 +0530

    mend

--- src/main/java/com/dimention/one/problems/Pro_3_Tile_The_Lane.java
+++ src/main/java/com/dimention/one/problems/Pro_3_Tile_The_Lane.java
@@ -1,6 +1,14 @@
 //$Id$
 package com.dimention.one.problems;
 
-public class Pro_3_Tile_The_Lane {
+/**
+ * Given a lane of 2*n length and an infinite supply of 1*2 tiles
+ * In how many ways lane can be tiled up
+ * 
+ * @author tarif-5329
+ *
+ */
 
+public class Pro_3_Tile_The_Lane {
+	
 }
